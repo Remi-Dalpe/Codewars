c061caa0a586b189adbb73c3fb078d123951a7fa
D1093N3 <remidalpe@hotmail.com>
2024-09-13 01:44:46 -0400

    Sept. 13

--- Range-Extraction-4kyu/script.js
+++ Range-Extraction-4kyu/script.js
@@ -2,30 +2,33 @@
 
 function solution(list) {
   let result = [];
-  let start = list[0];
-  let end = list[0];
-  // Iterate list from second element
-  for (let i = 1; i <= list.lenght; i++) {
-    if (list[i] === end + 1) {
-      //If number is consecutive, extend range
-      end = list[i];
+  let rangeStart = list[0];
+  let rangeEnd = list[0];
+
+  for (let i = 1; i < list.length; i++) {
+    if (list[i] === rangeEnd + 1) {
+      rangeEnd = list[i];
     } else {
-      //If number is not consecutive, end range & start new range
-      if (start === end) {
-        // Single value
-        result.push(start.toString());
-      } else if (end === start + 1) {
-        // Two number range, format separately
-        result.push(start.toString());
-        result.push(end.toString());
+      if (rangeEnd - rangeStart >= 2) {
+        result.push(`${rangeStart}-${rangeEnd}`);
+      } else if (rangeEnd - rangeStart === 1) {
+        result.push(`${rangeStart},${rangeEnd}`);
       } else {
-        // Range of more than 2 numbers
-        result.push(`${start}-${end}`);
+        result.push(`${rangeStart}`);
       }
-      // Start new range
-      start = list[i];
-      ens = list[i];
+      rangeStart = list[i];
+      rangeEnd = list[i];
     }
   }
+
+  // Handle the final range or single number
+  if (rangeEnd - rangeStart >= 2) {
+    result.push(`${rangeStart}-${rangeEnd}`);
+  } else if (rangeEnd - rangeStart === 1) {
+    result.push(`${rangeStart},${rangeEnd}`);
+  } else {
+    result.push(`${rangeStart}`);
+  }
+
   return result.join(',');
 }
